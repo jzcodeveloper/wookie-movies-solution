4b56f8fec2404a420eca7e0d6c9a371dec54658d
unknown <javier_bislip@hotmail.com>
2021-07-24 12:28:53 -0400

    First commit

--- src/redux/rootReducer.ts
+++
@@ -1,5 +0,0 @@
-import moviesReducer from "./movies";
-
-const rootReducer = { movies: moviesReducer };
-
-export default rootReducer;
--- src/redux/rootSaga.ts
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { moviesWatchers } from "./movies/watchers";
-
-export default function* rootSaga() {
-  yield all([...moviesWatchers]);
-}
